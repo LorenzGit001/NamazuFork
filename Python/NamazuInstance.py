5df383596fa5f58b69816981b4159c62d9fa3b06
LorenzGit001 <lorenz.baer@stud.uni-hannover.de>
2026-06-07 18:17:47 +0200

    Update steps_per_mm to 80stp/mm for DOLD axis
    Increase steps_per_mm for DOLD linear axis.

--- Python/NamazuInstance.py
+++ Python/NamazuInstance.py
@@ -8,6 +8,7 @@ class NamazuInstance:
         self.motorStartupDelay : float = 5.0 
         self.motionStartupDelay : float = 5.0
         self.motorRate : float = 100.0
+        # Up steps_per_mm to 80stp/mm for DOLD linear achse?
         self.steps_per_mm : float = 27.0
         self.comport : str = comport
         self.baudrate : int = 921600
